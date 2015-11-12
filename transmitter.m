7425bf18a0df5295f23ff2a8c3234dc4545c8a2d
edwawrdus <oskar.edwertz@gmail.com>
2015-11-12 14:19:38 +0100

    removed some crap

--- transmitter.m
+++ transmitter.m
@@ -11,7 +11,7 @@ bits = randsrc(1,2*N,[1 0]); %Random source of bits (length 2*N).
 
 GroupBits = buffer(bits,m)'; %Here we group the bits 2 and 2.
 
-codeword = bi2de(GroupBits,'left-msb')+1 %Assign each "group" to a decimal
+codeword = bi2de(GroupBits,'left-msb')+1; %Assign each "group" to a decimal
 
 symbols = QPSK(codeword);
 
@@ -25,5 +25,4 @@ OFDM = ifft(symbols,N);
 out = [];
 
 end
->>>>>>> 6ec99dc9b15b04b7152157e4534af43c565e8577
 
