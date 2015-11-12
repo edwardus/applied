45848185b773d13d6e532e4affc63a05a0ba0655
pkielen <pontus.kielen@gmail.com>
2015-11-12 14:15:57 +0100

    merged

--- transmitter.m
+++ transmitter.m
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-%% Transmitter
-%% Create bit stream
-
-b= %bitstream (k)
-%% QPSK
-
-s= %symbols (k)
-%% OFDM
-
-z=
-%% Send through channel
-=======
 function out = transmitter()
 %Transmitter file
 
