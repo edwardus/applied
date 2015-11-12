37cecf0893ca73fd7a70a1c996be05bf2574aa4f
edwawrdus <oskar.edwertz@gmail.com>
2015-11-12 15:20:30 +0100

    transmitter is more or less done

--- transmitter.m
+++ transmitter.m
@@ -1,28 +1,27 @@
-function out = transmitter()
+function z = transmitter()
 %Transmitter file
 
 %% Parameters
 N = 128;
 m= 2;
-QPSK = [1+1i; 1-1i; -1+1i; -1-1i];
+L=128; %Length of the cyclic prefix
+QPSK = [1+1i; 1-1i; -1+1i; -1-1i]./sqrt(2);
 
 %% Script
 bits = randsrc(1,2*N,[1 0]); %Random source of bits (length 2*N).
 
 GroupBits = buffer(bits,m)'; %Here we group the bits 2 and 2.
 
-codeword = bi2de(GroupBits,'left-msb')+1; %Assign each "group" to a decimal
+codeword = bi2de(GroupBits,'left-msb')+1; %Assign each "group" to a decimal.
 
-symbols = QPSK(codeword);
+symbols = QPSK(codeword); %Each number is assigned to our constellation.
 
-OFDM = ifft(symbols,N);
+OFDM = ifft(symbols,N); %We apply inverse-fft on our symbols (OFDM).
 
+Prefix = OFDM(end-L+1:end); %Cyclic prefix: Gimics a infinite time-signal
+                            %and works as a guard intervall.
+z = [Prefix;OFDM]; % adds the prefix to the signal.
 
-
-
-
-
-out = [];
 
 end
 
