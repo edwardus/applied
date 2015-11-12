6b4c6e7257a0311006eef223012dd09cc7d82675
edwawrdus <oskar.edwertz@gmail.com>
2015-11-12 12:27:13 +0100

    project is now started

--- transmitter.m
+++ transmitter.m
@@ -1,1 +1,21 @@
+function out = transmitter()
 %Transmitter file
+
+%% Parameters
+N = 128;
+m= 2;
+
+
+%% Script
+bits = randsrc(1,2*N,[1 0]); %Random source of bits (length 2*N).
+
+GroupBits = buffer(bits,m)'; %Here we group the bits 2 and 2.
+
+codeword = bi2de(GroupBits,'left-msb')+1; %Assign each "group" to a decimal
+
+
+
+
+out = [];
+
+end
