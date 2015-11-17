fd94ea1fd32fca47d6e88bd3a438d86845b63338
edwawrdus <oskar.edwertz@gmail.com>
2015-11-17 19:45:39 +0100

    added conversion to our bit layout

--- receiver.m
+++ receiver.m
@@ -3,7 +3,7 @@ function b_hat = receiver(y_hat,h,y_len)
 %% Parameters
 m=2;
 N=128;
-M=length(h); %Make sure we use the same L in transmitter and receiver
+M=length(h); %Make sure we use the same M in transmitter and receiver
 
 
 %% Processing
@@ -17,12 +17,17 @@ h_hat = fft(h,N);
 y_hatl=length(y_hat)
 r=fft(y_hat);
 rl=length(r)
-s_hat=r.*conj(h_hat)';
-b_hat=zeros(1,256)
-for n=1:128
-    b_hat((n*2-1))=sign(real(s_hat(n)));
-    b_hat((n*2))=sign(imag(s_hat(n)));
-end
+s_hat=r.*conj(h_hat)'
+b_hat=zeros(1,2*N);
+
+    for n=1:N
+     b_hat((n*2-1))=sign(real(s_hat(n)));
+     b_hat((n*2))=sign(imag(s_hat(n)));
+    end
+b_hat(b_hat==0)=3; %just for debugging
+b_hat(b_hat==-1)=0; %replace the -1 values
+
+    
 end
 
 
