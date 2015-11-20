355a6a72a991359664e8813f4d68b250c711c20f
pkielen <pontus.kielen@gmail.com>
2015-11-20 13:22:23 +0100

    channel

--- channel.m
+++ channel.m
@@ -1,8 +1,9 @@
-function [y_hat,h,sigma] = channel(z,mode,sigma)
+function [y_hat,y_hat_p,h,sigma] = channel(z,z_p,mode,sigma)
 
 %% Filter through channel impulse response and add noise
 sigma; %Noise level
 N=128;
+y_hat_p=[];
     if mode==1
        
       h=zeros(1,60); 
@@ -25,8 +26,11 @@ y_len = length(z)+length(h)-1;
 w = 1/sqrt(2)*sigma*(randn(y_len,1) + 1i*randn(y_len,1)); %Additive noise
 
 y_hat=conv(z,h)+w;
+if length(z_p)>1;
+    y_hat_p=conv(z_p,h)+w;
+end
 figure(1)
-freqz(fft(h))
+freqz(fft(h,N))
 % figure(2)
 % plot(real(y_hat))
 % title('Transmitted signal passed through h')
